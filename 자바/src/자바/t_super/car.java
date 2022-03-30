452f15ae653abb75cba253b268d0a4c1c40e6682
beans3142 <greenbeans3142@naver.com>
2022-03-30 23:32:45 +0900

    java studying..

---
+++ 자바/src/자바/t_super/car.java
@@ -0,0 +1,8 @@
+package ÀÚ¹Ù;
+
+public class car {
+	public car()
+	{
+		System.out.println("brr.. brr.. car's basic constructor");
+	}
+}
---
+++ 자바/src/자바/t_super/truck.java
@@ -0,0 +1,12 @@
+package t_super;
+
+public class truck extends car{
+	public truck() {
+		int size=15;
+		System.out.println("Truck's size is "+size);
+	}
+	public void main(String[] args)
+	{
+		int i = 5;
+	}
+}
